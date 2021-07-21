2d5f88b80f22d500c55b67d9a6ca59c3ce0cf0bb
John Long <johnlonggately@gmail.com>
2021-07-21 10:39:56 +0100

    Initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import css from './App.module.css';
+import Sidebar from "./components/Sidebar";
+import NavBarSimple from './components/NavBarSimple';
+import NavBarForm from "./components/NavBarForm";
+import Content from './components/Content';
+
+function App() {
+  return (
+    <div className={css.App}>
+      {/* Add your components here */}
+      <NavBarForm />
+      <Sidebar />
+      <Content />
+    </div>
+  );
+}
+
+export default App;
